16ec959448a4e0706ec35df87404a24c4d55f202
Pablo H. Paladino <paladinop@gmail.com>
2015-09-03 14:02:46 -0300

    commit inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sueldometroApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sueldometroApp
+ */
+angular.module('sueldometroApp')
+  .controller('AboutCtrl', function () {
+
+  	
+
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sueldometroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sueldometroApp
+ */
+angular.module('sueldometroApp')
+  .controller('MainCtrl', function () {
+
+  	
+
+  });
